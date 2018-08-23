78c15bb2cb828d57b3b90f6efeda6c43cdd8828d
Collin Matthew Sipple <csipple>
2018-08-23 11:54:58 -0500

    hello world file

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Collin Sipple, Minh Nguyen (Victor)
+ * Date: 2018/08/22
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Collin Sipple and I am a Computer Science Major!\n");
+
+  return 0;
+}
